2091ecb8b91c0ad107429104d2e04181c8ae972b
RenyBlanco <rey.blanco@yahoo.com>
2023-02-04 13:50:15 -0300

    Trabajando en un solo paciente

--- js/Consultorio.js
+++ js/Consultorio.js
@@ -9,12 +9,20 @@ function Consultorio() {
         return nombre;
     }
 
-    this.buscarPaciente = (nombre)=>{
-
+    this.buscarPaciente = (lista, nome)=>{
+        const result = lista.find(({ nombre }) => nombre === nome);
+        if(result){
+            return `
+            <div>
+            <p>Nombre :${result.nombre}</p>
+            <p>Apellido :${result.apellido}</p>
+            <p>Rut :${result.rut}</p>
+            <p>Edad :${result.edad}</p>
+            <p>Diagnostico :${result.diagnostico}</p>
+            </div>
+            `;
+        }else{
+            alert("Paciente no encontrado");
+        }
     }
-    this.datos = ()=>{
-        `Nombre : ${nombre}, Apellido: ${apellido}, Edad: ${edad}, Diagnostico: ${diagnostico}`
-
-    }
-
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,70 @@
-$(function() {
     var tabla;
-    // $.getJSON('js/listado.json', function(data) {
-    //     tabla = data.tabla;
-    //     iniciar(0);
-    // });
+    var pacientes = [];
+    $.getJSON('js/listado.json', function(data){
+        tabla = data.tabla.filas;
+        for(var i=0; i<tabla.length; i++){
+            var paciente = new Paciente();
+            paciente.setNombre = tabla[i].nombre;
+            paciente.setApellido = tabla[i].apellido;
+            paciente.setEdad = tabla[i].edad;
+            paciente.setRut = tabla[i].rut;
+            paciente.setDiagnostico = tabla[i].diagnostico;
+            pacientes.push(paciente);
+        }
+    });
+    
+    function mostrar(){
+        var card = `
+            <div class="card" >
+                <div class="card-header">
+                    <div class="float-left">
+                        <h4 style="color: gray;"><i class="fa-solid fa-hospital-user"></i> Pacientes</h4>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Rut</th>
+                                <th>Edad</th>
+                                <th>Diagnostico</th>
+                            </tr>
+                        </thead>
+                        <tbody id="cuerpo">
+                        </tbody>
+                        
+                    </table>
+                </div> <!-- /.card-body -->
+            </div> <!-- /.card -->
+        `
+        $("#tabla").html(card);
+        $("#cuerpo").html("");
+        for(var i=0; i<tabla.length; i++){
+            
+            var tr = `<tr>
+            <td>${tabla[i].nombre}</td>
+            <td>${tabla[i].apellido}</td>
+            <td>${tabla[i].rut}</td>
+            <td>${tabla[i].edad}</td>
+            <td>${tabla[i].diagnostico}</td>
+            </tr>`;
+            $("#cuerpo").append(tr)
+        }
+    }
 
-    // function iniciar(){
-    //     $("#tabla").empty();
-    //     jQuery.each(tabla.filas, function(i, fila) {
-    //         var f = "";
-    //         $.each(fila, function(index, valor){
-    //             if(index == "id"){
-    //                 f += "<tr><td>"+valor+"</td>"
-    //             }else{
-    //                 f += "<td>"+valor+"</td>"
-    //             }
-    //         });
-    //         f += `<td>
-    //                 <button type="button" class="btn btn-primary btn-sm" title="Detalle"><i class="fa-regular fa-address-book"></i></button>
-    //             </td>`
-    //         f += "</tr>"
-    //         $("#tabla").append(f);
-    //     });
-    // }
-
-
-    $(function(){ 
-        //$(".cargar").click(function(){
-          $.getJSON('js/listado.json', function(data){
-            tabla = data.tabla.filas;
-            console.log(tabla.filas);
-            $("#tabla").html("");
-            for(var i=0; i<tabla.length; i++){
-              var tr = `<tr>
-                <td>${tabla[i].id}</td>
-                <td>${tabla[i].nombre}</td>
-                <td>${tabla[i].apellido}</td>
-                <td>${tabla[i].rut}</td>
-                <td>${tabla[i].edad}</td>
-                <td>${tabla[i].diagnostico}</td>
-              </tr>`;
-              $("#tabla").append(tr)
-            }
-          });
-        })
-      //})
-});
+    function buscar(){
+        const consulta = new Consultorio;
+        consulta.buscarPaciente(pacientes, document.getElementById("nombre").value);
+        `
+        <div class="col-9">
+            <div class="form-group row">
+                <label for="nombre" class="col-2 col-form-label">Nombre :</label>
+                <div class="col-6">
+                    <input type="text" name="nombre" id="nombre" placeholder="Nombre paciente" autocomplete="off">
+                </div>
+            </div>
+        </div>
+        `
+    }
